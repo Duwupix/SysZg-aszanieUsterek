e8519952d6bf0cf7671e6b487fde686b34608542
Wodzirej33 <budziszmichal96@gmail.com>
2026-04-19 15:06:18 +0200

    Initial commit

---
+++ src/main/java/com/usterki/SystemUsterekApplication.java
@@ -0,0 +1,13 @@
+package com.usterki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemUsterekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemUsterekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usterki/SystemUsterekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usterki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemUsterekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
